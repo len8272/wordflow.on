d58a43fa7a783f9f7f7ed0cf33a8585789c95222
Elena Dorokhina <56249089+len8272@users.noreply.github.com>
2023-02-06 20:01:49 +0500

    one

--- pages/locators.py
+++ pages/locators.py
@@ -1,17 +1,17 @@
-import os
-from pages.base import WebPage
-# from pages.elements import WebElement
-
-
-class MainPage(WebPage): 
-
-    def __init__(self, web_driver, url=''):
-        if not url:
-            url = os.getenv("MAIN_URL") or https://wordflow.online/signin'
-
-        super().__init__(web_driver, url)
-
-    """Авторизация"""
+# import os
+# from pages.base import WebPage
+# # from pages.elements import WebElement
+#
+#
+# class MainPage(WebPage):
+#
+#     def __init__(self, web_driver, url=''):
+#         if not url:
+#             url = os.getenv("MAIN_URL") or https://wordflow.online/signin'
+#
+#         super().__init__(web_driver, url)
+#
+#     """Авторизация"""
 
     # login_field = WebElement(xpath='//*[@class="full-input__input formvalidate-error"]')
     # enter = WebElement(xpath='//*[@id="g-recap-0-btn"]')
--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -2,11 +2,14 @@
 
 # Для запуска тестов этойго файла ввести в терминал:
 # pytest -v --driver Chrome --driver-path \projects\chromedriver2.exe tests\test_auth_page.py
+# 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    github
 
-
+# import pytest
+# from selenium import webdriver
 import pytest
+import time
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def driver():
@@ -20,17 +23,17 @@ def page(driver):
     yield driver.current_url
     driver.quit()
 
-
+# положительный тест
 def test_signup_on_site_positive(driver, page):
     assert "https://wordflow.online/signup" in driver.current_url
 
     email_field = driver.find_element_by_xpath("//input[@name='email']")
-    email_field.send_keys("test@test.com")
+    email_field.send_keys("len8272@yandex.ru")
 
     password_field = driver.find_element_by_xpath("//input[@name='password']")
     password_field.send_keys("test123")
 
-    confirm_password_field = driver.find_element_by_xpath("//input[@name='confirm']")
+    confirm_password_field = driver.find_element_by_xpath("//input[@name='confirm']") #подтверждение пароля
     confirm_password_field.send_keys("test123")
 
     terms_checkbox = driver.find_element_by_xpath("//div[@class='checkbox']//input")
@@ -61,9 +64,46 @@ def test_signup_on_site_negative(driver, page):
     error_message = driver.find_element_by_xpath("//div[@class='Error']").text
     assert error_message == "Passwords must be at least 8 characters long"
 
-"""Тестируем возможность входа  в личный кабинет с корректным и некорректным логином (код скидки). 4 теста."""
+Импорт необходимых модулей
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-"""Вход с корретными данными"""
+# Драйвер
+selenium_driver = webdriver.Chrome()
+
+
+@pytest.fixture
+def log_in():
+    # устанавливаем параметры для окна и открываем сайт
+    selenium_driver.maximize_window()
+    selenium_driver.get("https://wordflow.online/signup")
+
+    # Находим элементы
+    login = selenium_driver.find_element(By.ID, "for_login")
+    password = selenium_driver.find_element(By.ID, "for_password")
+    log_in_button = selenium_driver.find_element(By.ID, "login-btn")
+
+    # Заполняем форму
+    login.send_keys("some_login")
+    password.send_keys("some_password@11")
+    log_in_button.click()
+    time.sleep(5)
+
+    yield
+
+    # Выходим из аккаунта, закрываем браузер и полностью завершаем тест
+    selenium_driver.quit()
+
+
+def test_log_in(log_in):
+    # Проверяем, что мы успешно залогинились
+    assert selenium_driver.title == "WordFlow — The world’s fastest way to write"
+
+# """Тестируем возможность входа  в личный кабинет с корректным и некорректным логином (код скидки). 4 теста."""
+#
+# """Вход с корретными данными"""
 #
 # def test_valid_login_auth_page(web_browser):
 #     page = MainPage(web_browser)
@@ -71,7 +111,7 @@ def test_signup_on_site_negative(driver, page):
 #     page.login_field.send_keys("1208-41C9-80DB")
 #     page.enter.click()
 #     page.automatic_closing.click()
-#
+# #
 #     assert page.get_current_url() == 'https://www.labirint.ru/'
 #
 # """Вход с некорретными данными"""
